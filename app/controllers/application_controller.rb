875d03a662e91a421adc110b06b109e130f25921
AndriiBuhalo <andriubuhal@gmail.com>
2022-01-27 18:07:14 +0200

    add locales for registration, add edit registration, add permited param for devise (add first name... phone)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
   protect_from_forgery with: :null_session
 
@@ -13,5 +14,11 @@ class ApplicationController < ActionController::Base
   def set_locale
     locale_in_cookies = I18n.available_locales.map(&:to_s).include?(cookies[:locale])
     I18n.locale = locale_in_cookies ? cookies[:locale] : I18n.default_locale
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :phone, :email, :password, :password_confirmation) }
+
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :phone, :email, :password, :password_confirmation) }
   end
 end
