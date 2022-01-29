dc7453e1577ff8e5f4063878d35603a9379d4241
Andrii <andriubuhal@gmail.com>
2022-01-29 09:40:44 +0200

    add views on registration
     add locales for registration, add edit registration, add permited param for devise (add first name... phone)
    
     fix bug in new registration button
    
    change name for block value and change quotes in this file
    
    Fix rubocop issues

--- app/javascript/controllers/renderer_controller.js
+++ app/javascript/controllers/renderer_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
-  static targets = ['display']
+  static targets = ["display"]
 
   render(evt) {
     this.displayTarget.innerHTML = evt.detail[0].getElementsByClassName("content")[0].innerHTML
