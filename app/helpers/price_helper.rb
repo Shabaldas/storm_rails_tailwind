e856d15edfff5545a09167acd902866c19f8fb23
Volodymyr <shabaldasengr@gmail.com>
2022-05-31 18:50:49 +0300

    Fix price in cart items and product page

--- app/helpers/price_helper.rb
+++ app/helpers/price_helper.rb
@@ -2,6 +2,7 @@ module PriceHelper
   def total_price_second(currnet_cart)
     array = []
     currnet_cart.cart_items.each do |cart_item|
+      array << (cart_item.cartable.subtotal_price * cart_item.quantity) if cart_item.cartable_type == 'PrintModelAttribute'
       array << (cart_item.cart_item_option_values.sum(&:price) * cart_item.quantity) # rubocop/disable Lint/AmbiguousOperatorPrecedence
     end
     array.sum
