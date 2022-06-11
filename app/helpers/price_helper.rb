40845920412208acfd283fcd07aaf75c84675739
Volodymyr <shabaldasengr@gmail.com>
2022-06-11 13:11:35 +0300

    Fix price helper

--- app/helpers/price_helper.rb
+++ app/helpers/price_helper.rb
@@ -2,8 +2,11 @@ module PriceHelper
   def total_price_second(currnet_cart)
     array = []
     currnet_cart.cart_items.each do |cart_item|
-      array << (cart_item.cartable.subtotal_price * cart_item.quantity) if cart_item.cartable_type == 'PrintModelAttribute'
-      array << (cart_item.cart_item_option_values.sum(&:price) * cart_item.quantity) # rubocop/disable Lint/AmbiguousOperatorPrecedence
+      if cart_item.cartable_type == 'PrintModelAttribute' # rubocop:disable Style/ConditionalAssignment
+        array << (cart_item.cartable.subtotal_price * cart_item.quantity)
+      else
+        array << (cart_item.cart_item_option_values.sum(&:price) * cart_item.quantity)
+      end
     end
     array.sum
   end
