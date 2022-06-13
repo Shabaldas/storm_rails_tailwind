afaedee11414f48bfcc7313b5425224cd042bcb1
Volodymyr <shabaldasengr@gmail.com>
2022-06-13 15:59:38 +0300

    Add test phone question error

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,16 +10,14 @@ class StaticPagesController < ApplicationController
   def print; end
 
   def save_phone_number
-    @call_question = CallQuestion.new
-    ap @call_question
-    ap @call_question.assign_attributes(call_question_params)
-   ap @call_question.valid?
-   pp @call_question.errors.full_messages
-    # respond_to do |format|
-    #   format.turbo_stream do
-    #     render turbo_stream: turbo_stream.replace('print_model_id_field', partial: 'question_form')
-    #   end
-    # end
+    @call_question = CallQuestion.new(call_question_params)
+    respond_to do |format|
+      format.turbo_stream do
+        unless @call_question.save
+          render turbo_stream: turbo_stream.update('print_model_id_field', partial: 'question_form')
+        end
+      end
+    end
   end
 
   private
