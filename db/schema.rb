21fb3bad2f0e57b26f84024400ccbfbb74348199
Volodymyr <shabaldasengr@gmail.com>
2022-06-28 23:16:43 +0300

    Fix schema file

--- db/schema.rb
+++ db/schema.rb
@@ -132,17 +132,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_11_174441) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "order_items", force: :cascade do |t|
-    t.integer "quantity"
-    t.bigint "product_id", null: false
-    t.bigint "order_id", null: false
-    t.decimal "total"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["order_id"], name: "index_order_items_on_order_id"
-    t.index ["product_id"], name: "index_order_items_on_product_id"
-  end
-
   create_table "orders", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -256,12 +245,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_11_174441) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "widgets", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "cart_item_option_values", "cart_items", on_delete: :cascade
@@ -269,7 +252,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_11_174441) do
   add_foreign_key "cart_items", "carts"
   add_foreign_key "carts", "orders", on_delete: :cascade
   add_foreign_key "option_values", "options", on_delete: :cascade
-  add_foreign_key "order_items", "products"
   add_foreign_key "print_model_attributes", "print_models", on_delete: :cascade
   add_foreign_key "product_images", "products", on_delete: :cascade
   add_foreign_key "product_option_values", "option_values", on_delete: :cascade
